61c75e61f5321a048f4f77a7e83bca955e3e4945
Juscelino Cordeiro <jfilhodev@gmail.com>
2022-07-29 19:29:16 -0300

    commit inicial

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    phpVersion: '8.1',
+    trailingCommaPHP: true,
+    braceStyle: 'psr-2',
+    printWidth: 130,
+    vueIndentScriptAndStyle: true,
+};
